3a5e23be531b2284ead3d8bef44b205699266329
David Van Der Beek <earlynovrock@gmail.com>
2013-05-17 11:54:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maven::Application.config.secret_token = '411354bf2d57a49d71d6943b343e5c8bb27e2cd38d8bbbe85c84ae3b779b3a7eefd9f14ca460a257603c4c5c66a611cf0d447258eb25b4f47d68803ef0920851'

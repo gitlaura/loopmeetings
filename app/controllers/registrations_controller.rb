60132d00a5dd3afc9e316e315d9a3e8143a8e450
David Van Der Beek <earlynovrock@gmail.com>
2013-05-20 11:41:01 -0500

    update devise without password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,35 @@
 class RegistrationsController < Devise::RegistrationsController
+  def update
+    @user = User.find(current_user.id)
 
-  protected
-
-    def after_update_path_for(resource)
-      edit_user_registration_path
+    successfully_updated = if needs_password?(@user, params)
+      @user.update_with_password(params[:user])
+    else
+      # remove the virtual current_password attribute update_without_password
+      # doesn't know how to ignore it
+      params[:user].delete(:current_password)
+      @user.update_without_password(params[:user])
     end
+
+    if successfully_updated
+      set_flash_message :notice, :updated
+      # Sign in the user bypassing validation in case his password changed
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+
+protected
+
+	def needs_password?(user, params)
+    user.email != params[:user][:email] ||
+      !params[:user][:password].blank?
+  end
+  
+  def after_update_path_for(resource)
+    edit_user_registration_path
+  end
 
 end

585677ca6d085d6e4e45114cf4c808052a9a699c
Dave Shapiro <dss.shapiro@gmail.com>
2013-06-17 16:53:36 -0500

    adding email field to invitation record

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -5,22 +5,31 @@ class InvitationsController < ApplicationController
 
   def create
 
-    unless params[:your_name].nil?
+    unless params[:your_email].nil? || params[:your_email].blank?
+      user = User.find_or_initialize_by(email: params[:your_email])
+    else
+      user = User.new
+    end
 
-      name_array = params[:your_name].split
-      user = User.find_or_initialize_by(first_name: name_array[0] , last_name: name_array[1] )
-      user.save(validate: false)
+    name_array = params[:your_name].split unless params[:your_name].nil?
+    if user.first_name.nil? && !name_array.nil?
+      user.first_name = name_array[0]
+      user.last_name = name_array[1]
+    end
 
-      unless params[:invite_email].nil?
-        params[:invite_email].each do |e|
-          unless e.blank?
-            inv = user.invitations.create! email: e
-            InvitationWorker.perform_async(inv.id.to_s)
-          end
+    user.save(validate: false)
+
+
+    unless params[:invite_email].nil?
+      params[:invite_email].each do |e|
+        unless e.blank?
+          inv = user.invitations.create! email: e
+          InvitationWorker.perform_async(inv.id.to_s)
         end
       end
-
     end
+
+
     redirect_to invitations_path
   end
 
